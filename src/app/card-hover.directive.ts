b3f284b7bf6fcc5b41e15dc4566eb6c4a37de904
louie1966 <renedeleeuw66@gmail.com>
2017-08-16 15:36:19 +0200

    HostListener & HostBinding Code

--- src/app/card-hover.directive.ts
+++ src/app/card-hover.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[ccCardHover]'
+  selector: "[ccCardHover]"
 })
 export class CardHoverDirective {
+  @HostBinding('class.card-outline-primary')private ishovering: boolean;
+
   constructor(private el: ElementRef,
-    private renderer: Renderer) {
-    //noinspection TypeScriptUnresolvedVariable,TypeScriptUnresolvedFunction
-    renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
+              private renderer: Renderer) {
+    // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
+  }
+
+  @HostListener('mouseover') onMouseOver() {
+    let part = this.el.nativeElement.querySelector('.card-text');
+    this.renderer.setElementStyle(part, 'display', 'block');
+    this.ishovering = true;
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    let part = this.el.nativeElement.querySelector('.card-text');
+    this.renderer.setElementStyle(part, 'display', 'none');
+    this.ishovering = false;
   }
 }
